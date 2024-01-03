9b2e2abe613df382249127700cc8b0070d2267e5
Alec Thao <thaoalec93@gmail.com>
2024-01-02 19:48:07 -0700

    added all the code, push and submit the code

--- CKK.Logic/Customer.cs
+++ CKK.Logic/Customer.cs
@@ -5,6 +5,36 @@
         private int Id;
         private string Name;
         private string Address;
+
+        public int getId()
+        {
+            return Id;
+        }
+
+        public void SetId(int Id)
+        {
+            this.Id = Id;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public void SetName(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public string GetAddress()
+        {
+            return Address;
+        }
+
+        public void SetAddress(string Address)
+        {
+            this.Address = Address;
+        }
     }
 
 }
--- CKK.Logic/Product.cs
+++ CKK.Logic/Product.cs
@@ -11,5 +11,35 @@ namespace CKK.Logic
         private int Id;
         private string Name;
         private decimal Price;
+
+        public int GetId()
+        {
+            return Id;
+        }
+
+        public void SetId(int Id)
+        {
+            this.Id = Id;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public void SetName(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public decimal GetPrice()
+        {
+            return Price;
+        }
+
+        public void SetPrice(int Price)
+        {
+            this.Price = Price;
+        }
     }
 }
