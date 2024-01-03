3678e5107631dc06312b3179ae6f9e000fa0577d
Alec Thao <thaoalec93@gmail.com>
2024-01-02 19:34:48 -0700

    changed shoppingcartitem and updated it

--- CKK.Logic/ShoppingCartItem.cs
+++ CKK.Logic/ShoppingCartItem.cs
@@ -8,7 +8,38 @@ namespace CKK.Logic
 {
     internal class ShoppingCartItem
     {
-        private Product Product;
-        private int Quantity;
+        private Product _product;
+        private int _quantity;
+
+        public ShoppingCartItem(Product product, int quantity)
+        {
+            _product = product;
+            _quantity = quantity;
+        }
+
+        public int GetQuantity(int quantity)
+        {
+            return _quantity;
+        }
+
+        public void SetQuantity(int quantity)
+        {
+            this._quantity = quantity;
+        }
+
+        public Product GetProduct(Product product)
+        {
+            return _product;
+        }
+
+        public void SetProduct(Product product)
+        {
+            this._product = product;
+        }
+
     }
+
+     
+
+    
 }
