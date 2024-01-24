6f40d2ac24ce573d21f650fc7c587ef0d6bae0be
Alec Thao <thaoalec93@gmail.com>
2024-01-23 18:00:17 -0700

    adjusted shopping cart item

--- CKK.Logic/Models/ShoppingCartItem.cs
+++ CKK.Logic/Models/ShoppingCartItem.cs
@@ -39,7 +39,7 @@ namespace CKK.Logic.Models
 
         public decimal GetTotal()
         {
-            decimal total = Convert.ToDecimal(_quantity) *  Convert.ToDecimal(_product);
+            decimal total = Convert.ToDecimal(_quantity *  _product);
             return total;
         }
 
