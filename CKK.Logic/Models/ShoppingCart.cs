658e3d3ce1548ddd3c88094171023bc745489219
Alec (Home PC) <Alec@DESKTOP-TVRJ39G>
2024-02-08 22:18:40 -0700

    added shopping cart added GetCustomerId(), GetDecimal(), GetTotal(), GetProduct(), GetProductById()
    GetProduct() is unfinished
    constructor is unfinished

---
+++ CKK.Logic/Models/ShoppingCart.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKK.Logic.Models
+{
+    internal class ShoppingCart
+    {
+        private string _customer;
+        private Product _product1;
+        private Product _product2;
+        private Product _product3;
+
+        public ShoppingCart(Customer customer)
+        {
+            _customer = customer;
+        }
+
+        public int GetCustomerId()
+        {
+            return 3;
+        }
+
+        public decimal GetDecimal()
+        {
+            return 0.0m;
+        }
+
+        public decimal GetTotal()
+        {
+            return 0.0m;
+        }
+
+        public int GetProduct(ShoppingCartItem prodNum)
+        {
+            return 3;
+        }
+        public int GetProductById(int id)
+        {
+            return 3;
+        }
+
+    }
+}
