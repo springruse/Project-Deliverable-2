14329ad687855dd2c611f8d26864e79ef1f399ff
Alec Thao <thaoalec93@gmail.com>
2024-04-24 19:06:21 -0600

    fixed everything possibly???

--- CKK.Logic/Models/Store.cs
+++ CKK.Logic/Models/Store.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Dynamic;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace CKK.Logic.Models
 {
@@ -82,7 +83,7 @@ namespace CKK.Logic.Models
 
             if (additem != null)
             {
-                if (additem.GetQuantity() - quantity <= 0)
+                if (additem.GetQuantity() - quantity > 0)
                 {
                     additem.SetQuantity(additem.GetQuantity() - quantity);
                     return additem;
