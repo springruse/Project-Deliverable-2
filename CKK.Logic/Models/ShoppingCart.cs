bf4b93cfcff78369be59a2dd74d212b5aa235ca1
Alec Thao <thaoalec93@gmail.com>
2024-06-20 19:15:19 -0600

    Fixed ShoppingCart

--- CKK.Logic/Models/ShoppingCart.cs
+++ CKK.Logic/Models/ShoppingCart.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CKK.Logic.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Metadata;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace CKK.Logic.Models
 {
-    public class ShoppingCart
+    public class ShoppingCart : InventoryItem
     {
         public Customer Customer { get; set; }
         public Product Products { get; set; }
